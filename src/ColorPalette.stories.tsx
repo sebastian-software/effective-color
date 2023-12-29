aa704be3f950ef8573d0f644c295013b80a56024
Sebastian Werner <s.werner@sebastian-software.de>
2023-12-29 14:19:54 +0100

    worked on color demos

--- src/ColorPalette.stories.tsx
+++ src/ColorPalette.stories.tsx
@@ -4,13 +4,18 @@ import {
   converter,
   differenceCiede2000,
   formatCss,
+  formatHex,
   interpolate,
   modeOklab
 } from "culori"
 import { useMemo, type FC, type PropsWithChildren } from "react"
+import "./global.css"
 
 const meta: Meta<FC> = {
-  component: () => null
+  component: () => null,
+  parameters: {
+    layout: "fullscreen"
+  }
 }
 
 export default meta
@@ -21,6 +26,7 @@ function BadgeLayout({ children }: PropsWithChildren) {
   return (
     <div
       style={{
+        background: "black",
         display: "flex",
         flexDirection: "row",
 
@@ -64,7 +70,7 @@ function Badge({ background, text, label }: BadgeProps) {
 
 const differ = differenceCiede2000()
 
-function findNextShade(start: string, end: string, difference = 2) {
+function findNextShade(start: string, end: string, difference: number) {
   if (differ(start, end) < 0.1) {
     return
   }
@@ -80,11 +86,16 @@ function findNextShade(start: string, end: string, difference = 2) {
   }
 }
 
-function buildShades(start: string, end: string, steps: number) {
+function buildShades(
+  start: string,
+  end: string,
+  steps: number,
+  difference: number
+) {
   const result = []
   let current = start
   for (let i = 0; i < 100; i++) {
-    const next = findNextShade(current, end)
+    const next = findNextShade(current, end, difference)
     if (next) {
       result.push(next)
       current = next
@@ -104,20 +115,25 @@ interface ColorShapeProps {
   name: string
   to: string
   color: string
-  steps?: number
+  steps: number
+  difference: number
 }
 
-function ColorShades({ name, color, to, steps = 8 }: ColorShapeProps) {
-  const shades = useMemo(() => buildShades(to, color, steps), [color, steps])
+function ColorShades({ name, color, to, steps, difference }: ColorShapeProps) {
+  const shades = useMemo(
+    () => buildShades(to, color, steps, difference),
+    [color, steps]
+  )
 
   return (
     <BadgeLayout>
       <Badge background={color} text={to} label={name} />
+      <Badge background={to} text="#fff" label="to" />
       {shades.map((shade, index) => {
         return (
           <Badge
             key={index}
-            background={formatCss(shade)}
+            background={formatHex(shade)}
             text={color}
             label={"#" + index}
           />
@@ -128,53 +144,66 @@ function ColorShades({ name, color, to, steps = 8 }: ColorShapeProps) {
 }
 
 export function LightShades() {
+  const config = {
+    to: "#fff",
+    difference: 2,
+    steps: 7
+  }
+
   return (
     <>
       {/* Based on colors from https://tailwindcss.com/docs/customizing-colors */}
-      <ColorShades name="tw-neutral-700" color="#404040" to="#fff" />
-      <ColorShades name="tw-red-600" color="#dc2626" to="#fff" />
-      <ColorShades name="tw-orange-600" color="#ea580c" to="#fff" />
-      <ColorShades name="tw-amber-600" color="#d97706" to="#fff" />
-      <ColorShades name="tw-yellow-600" color="#ca8a04" to="#fff" />
-      <ColorShades name="tw-lime-600" color="#65a30d" to="#fff" />
-      <ColorShades name="tw-green-600" color="#16a34a" to="#fff" />
-      <ColorShades name="tw-emerald-600" color="#059669" to="#fff" />
-      <ColorShades name="tw-teal-600" color="#0d9488" to="#fff" />
-      <ColorShades name="tw-cyan-600" color="#0891b2" to="#fff" />
-      <ColorShades name="tw-sky-600" color="#0284c7" to="#fff" />
-      <ColorShades name="tw-blue-600" color="#2563eb" to="#fff" />
-      <ColorShades name="tw-indigo-600" color="#4f46e5" to="#fff" />
-      <ColorShades name="tw-violet-600" color="#7c3aed" to="#fff" />
-      <ColorShades name="tw-purple-600" color="#9333ea" to="#fff" />
-      <ColorShades name="tw-fuchsia-600" color="#c026d3" to="#fff" />
-      <ColorShades name="tw-pink-600" color="#db2777" to="#fff" />
-      <ColorShades name="tw-rose-600" color="#e11d48" to="#fff" />
+      <ColorShades name="tw-neutral-600" color="#525252" {...config} />
+      <ColorShades name="tw-red-600" color="#dc2626" {...config} />
+      <ColorShades name="tw-orange-600" color="#ea580c" {...config} />
+      <ColorShades name="tw-amber-600" color="#d97706" {...config} />
+      <ColorShades name="tw-yellow-600" color="#ca8a04" {...config} />
+      <ColorShades name="tw-lime-600" color="#65a30d" {...config} />
+      <ColorShades name="tw-green-600" color="#16a34a" {...config} />
+      <ColorShades name="tw-emerald-600" color="#059669" {...config} />
+      <ColorShades name="tw-teal-600" color="#0d9488" {...config} />
+      <ColorShades name="tw-cyan-600" color="#0891b2" {...config} />
+      <ColorShades name="tw-sky-600" color="#0284c7" {...config} />
+      <ColorShades name="tw-blue-600" color="#2563eb" {...config} />
+      <ColorShades name="tw-indigo-600" color="#4f46e5" {...config} />
+      <ColorShades name="tw-violet-600" color="#7c3aed" {...config} />
+      <ColorShades name="tw-purple-600" color="#9333ea" {...config} />
+      <ColorShades name="tw-fuchsia-600" color="#c026d3" {...config} />
+      <ColorShades name="tw-pink-600" color="#db2777" {...config} />
+      <ColorShades name="tw-rose-600" color="#e11d48" {...config} />
     </>
   )
 }
 
 export function DarkShades() {
+  const config = {
+    // "to" is not black but the first color cielab reports as visually
+    // different to black (difference=1)
+    to: "#060606",
+    difference: 2,
+    steps: 7
+  }
   return (
     <>
       {/* Based on colors from https://tailwindcss.com/docs/customizing-colors */}
-      <ColorShades name="tw-neutral-700" color="#404040" to="#000" />
-      <ColorShades name="tw-red-600" color="#dc2626" to="#000" />
-      <ColorShades name="tw-orange-600" color="#ea580c" to="#000" />
-      <ColorShades name="tw-amber-600" color="#d97706" to="#000" />
-      <ColorShades name="tw-yellow-600" color="#ca8a04" to="#000" />
-      <ColorShades name="tw-lime-600" color="#65a30d" to="#000" />
-      <ColorShades name="tw-green-600" color="#16a34a" to="#000" />
-      <ColorShades name="tw-emerald-600" color="#059669" to="#000" />
-      <ColorShades name="tw-teal-600" color="#0d9488" to="#000" />
-      <ColorShades name="tw-cyan-600" color="#0891b2" to="#000" />
-      <ColorShades name="tw-sky-600" color="#0284c7" to="#000" />
-      <ColorShades name="tw-blue-600" color="#2563eb" to="#000" />
-      <ColorShades name="tw-indigo-600" color="#4f46e5" to="#000" />
-      <ColorShades name="tw-violet-600" color="#7c3aed" to="#000" />
-      <ColorShades name="tw-purple-600" color="#9333ea" to="#000" />
-      <ColorShades name="tw-fuchsia-600" color="#c026d3" to="#000" />
-      <ColorShades name="tw-pink-600" color="#db2777" to="#000" />
-      <ColorShades name="tw-rose-600" color="#e11d48" to="#000" />
+      <ColorShades name="tw-neutral-600" color="#525252" {...config} />
+      <ColorShades name="tw-red-600" color="#dc2626" {...config} />
+      <ColorShades name="tw-orange-600" color="#ea580c" {...config} />
+      <ColorShades name="tw-amber-600" color="#d97706" {...config} />
+      <ColorShades name="tw-yellow-600" color="#ca8a04" {...config} />
+      <ColorShades name="tw-lime-600" color="#65a30d" {...config} />
+      <ColorShades name="tw-green-600" color="#16a34a" {...config} />
+      <ColorShades name="tw-emerald-600" color="#059669" {...config} />
+      <ColorShades name="tw-teal-600" color="#0d9488" {...config} />
+      <ColorShades name="tw-cyan-600" color="#0891b2" {...config} />
+      <ColorShades name="tw-sky-600" color="#0284c7" {...config} />
+      <ColorShades name="tw-blue-600" color="#2563eb" {...config} />
+      <ColorShades name="tw-indigo-600" color="#4f46e5" {...config} />
+      <ColorShades name="tw-violet-600" color="#7c3aed" {...config} />
+      <ColorShades name="tw-purple-600" color="#9333ea" {...config} />
+      <ColorShades name="tw-fuchsia-600" color="#c026d3" {...config} />
+      <ColorShades name="tw-pink-600" color="#db2777" {...config} />
+      <ColorShades name="tw-rose-600" color="#e11d48" {...config} />
     </>
   )
 }
